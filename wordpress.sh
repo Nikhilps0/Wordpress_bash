73fea23e40117c31efb324ae4cc2b1fbad33210b
Nikhilps0 <nikhil.jolju@gmail.com>
2022-06-19 22:11:41 +0530

    Final commit

--- wordpress.sh
+++ wordpress.sh
@@ -8,9 +8,9 @@ mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost',
 mysql -e "DROP DATABASE IF EXISTS test;"
 mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'"
 mysql -e "FLUSH PRIVILEGES;"
-mysql -u root -p'55zK4THjpdHzcMdU' -e "create database ${DATABASE_NAME};"
-mysql -u root -p'55zK4THjpdHzcMdU' -e "create user '${DATABASE_USER}'@'localhost' identified by '${DATABASE_PASSWORD}';"
-mysql -u root -p'55zK4THjpdHzcMdU' -e "grant all privileges on ${DATABASE_NAME}.* to '${DATABASE_USER}'@'localhost';"
+mysql -u root -p'55zK4THjpdHzcMdU' -e "create database wordpress;"
+mysql -u root -p'55zK4THjpdHzcMdU' -e "create user 'wordpress'@'localhost' identified by '55zK4THjpdHzcMdU';"
+mysql -u root -p'55zK4THjpdHzcMdU' -e "grant all privileges on wordpress.* to 'wordpress'@'localhost';"
 mysql -u root -p'55zK4THjpdHzcMdU' -e "flush privileges;"
 systemctl restart mariadb.service
 
@@ -33,13 +33,9 @@ wget https://wordpress.org/latest.tar.gz -P /var/website/
 tar -xf /var/website/latest.tar.gz -C /var/website/
 cp -r /var/website/wordpress/* /var/www/html/
 
-sed -i 's/database_name_here/${DATABASE_NAME}/g' /var/www/html/wp-config-sample.php
-sed -i 's/username_here/${DATABASE_USER}/g' /var/www/html/wp-config-sample.php
-sed -i 's/password_here/${DATABASE_PASSWORD}/g' /var/www/html/wp-config-sample.php
-sed -i 's/localhost/localhost/g' /var/www/html/wp-config-sample.php
-
-
-echo "SetEnvIf x-forwarded-proto https HTTPS=on" >> /etc/httpd/conf/httpd.conf
+sed -i 's/database_name_here/wordpress/g' /var/www/html/wp-config-sample.php
+sed -i 's/username_here/wordpress/g' /var/www/html/wp-config-sample.php
+sed -i 's/password_here/55zK4THjpdHzcMdU/g' /var/www/html/wp-config-sample.php
 
 sed -i 's/post_max_size = 8M/post_max_size = 128M/g'  /etc/php.ini
 sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 128M/g'  /etc/php.ini
